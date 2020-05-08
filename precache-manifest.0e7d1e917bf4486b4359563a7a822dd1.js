5bc21656554d73587a4ac08eb87811f34a83a713
Hector carrillo <hectorcarrillo@Hectors-MacBook-Pro.local>
2020-05-07 21:14:46 -0500

    Updates

---
+++ precache-manifest.0e7d1e917bf4486b4359563a7a822dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbcb963dc5ae732e75b88b901be8af1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e96d888446c44034fa0",
+    "url": "/monsters-rolodex/static/css/main.5a77c2fa.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e96d888446c44034fa0",
+    "url": "/monsters-rolodex/static/js/main.512cb846.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
